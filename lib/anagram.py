d1991fade0917c2fec69dd2c89811e2c13b01967
Nicole kibe <nicolekibe822@gmail.com>
2025-11-18 13:04:28 +0300

    commit all changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram :
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word.lower())  # store sorted lowercase letters
+
+    def match(self, candidates):
+        return [
+            candidate
+            for candidate in candidates
+            if candidate.lower() != self.word.lower() and sorted(candidate.lower()) == self.sorted_word
+        ]
